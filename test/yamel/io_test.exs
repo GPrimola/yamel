7383173071051860865b6207fdabdc34924b9888
Giorgio Torres <torres.giorgio@gmail.com>
2020-10-11 08:38:28 -0300

    Added tests for Yamel.IO.read

---
+++ test/yamel/io_test.exs
@@ -0,0 +1,36 @@
+defmodule Yamel.IOTest do
+  use ExUnit.Case
+
+  @valid_yml "test/fixtures/valid.yml"
+  @invalid_yml "test/fixtures/invalid.yml"
+
+  describe "read!/1" do
+    test "should read a valid YAML file" do
+      assert is_map(Yamel.IO.read!(@valid_yml))
+    end
+
+    test "should raise FileNotFoundError when file doesn't exist" do
+      assert_raise(YamlElixir.FileNotFoundError, fn ->
+        Yamel.IO.read!("file/doesnt/exist.yml")
+      end)
+    end
+
+    test "should raise ? with an invalid YAML file" do
+      assert_raise(YamlElixir.ParsingError, "malformed yaml", fn ->
+        Yamel.IO.read!(@invalid_yml)
+      end)
+    end
+  end
+
+  describe "read/1" do
+    test "should read a valid YAML file" do
+      assert {:ok, result} = Yamel.IO.read(@valid_yml)
+      assert is_map(result)
+    end
+
+    test "should return {:error, reason} with an invalid YAML file" do
+      assert {:error, reason} = Yamel.IO.read(@invalid_yml)
+      assert %YamlElixir.ParsingError{message: "malformed yaml"} = reason
+    end
+  end
+end
